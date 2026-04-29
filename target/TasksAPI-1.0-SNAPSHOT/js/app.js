ef22904f8a60335eda30e7981be068601e35f46d
Adrián Castán <151794030+adcastan@users.noreply.github.com>
2026-04-28 18:05:55 -0700

    styles

--- target/TasksAPI-1.0-SNAPSHOT/js/app.js
+++ target/TasksAPI-1.0-SNAPSHOT/js/app.js
@@ -2,46 +2,49 @@ import * as state from "./state.js"
 import * as ui from "./uiiii/ui.js"
 
 
-document.addEventListener('DOMContentLoader', () => {
-    const input = document.querySelector('taskInput');
-    const addBtn = document.getElementById('addBtn');
-    const list = document.getElementById("taskList");
+        document.addEventListener('DOMContentLoader', () => {
+            const input = document.querySelector('taskInput');
+            const addBtn = document.getElementById('addBtn');
+            const list = document.getElementById("taskList");
 
-    const tasks = await state.fetchTasks;
-    ui.render(tasks);
+            const tasks = await state.fetchTasks;
+            ui.render(tasks);
 
 
-    addBtn.addEventListener('click', async() => {
-        if (!input.value.trim()) return;
-        state.addTask(input.value());
-        input.value = "";
-        ui.render(state.tasks);
+            addBtn.addEventListener('click', async() => {
+                if (!input.value.trim())
+                    return;
+                state.addTask(input.value());
+                input.value = "";
+                ui.render(state.tasks);
 
-    });
+            });
 
-    input.addEventListener('keydown', async(e) => {
-        if (e.key == 'Enter') addTask();
-    })
+            input.addEventListener('keydown', async(e) => {
+                if (e.key == 'Enter')
+                    addTask();
+            })
 
-    list.addEventListener('click', (e) => {
-        const id = parseInt(e.target.closest('li').dataset.id);
+            list.addEventListener('click', (e) => {
+                const id = parseInt(e.target.closest('li').dataset.id);
 
-        if (e.target.classList.contains('btn-delete')) {
-            state.deleteTask(id);
-            return;
-        }
+                if (e.target.classList.contains('btn-delete')) {
+                    state.deleteTask(id);
+                    return;
+                }
 
-        if (e.target.classList.contains('btn-edit')) {
-            const task = tasks.find(t => t.id === id);
-            const newText = prompt("Editar: ", task.text);
-            if (newText) state.editTask(id, newText);
-            return;
-        }
+                if (e.target.classList.contains('btn-edit')) {
+                    const task = tasks.find(t => t.id === id);
+                    const newText = prompt("Editar: ", task.text);
+                    if (newText)
+                        state.editTask(id, newText);
+                    return;
+                }
 
-        if (e.target.tagName !== 'BUTTON') {
-            state.toggleTask();
-        }
+                if (e.target.tagName !== 'BUTTON') {
+                    state.toggleTask();
+                }
 
-        ui.render(state.tasks);
-    });
-});
+                ui.render(state.tasks);
+            });
+        });
--- target/TasksAPI-1.0-SNAPSHOT/js/state.js
+++ target/TasksAPI-1.0-SNAPSHOT/js/state.js
@@ -3,9 +3,9 @@ const API_URL = "http://localhost.8080/TasksAPI/api/task"
 export let tasks = [];
 
 
-export const fetchTasks= async ()=>{
+export const fetchTasks = async () => {
     const response = await fetch(API_URL);
-    tasks=await response.json();
+    tasks = await response.json();
     return tasks;
 };
 
@@ -13,8 +13,8 @@ export const fetchTasks= async ()=>{
 export const addTask = (text) => {
     const response = await fetch(API_URL, {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ text: text, completed: false })
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({text: text, completed: false})
     });
 
     const task = await response.json();
@@ -36,14 +36,15 @@ export const deleteTask = (text) => {
 export const toggleTask = (id) => {
 
     const task = tasks.find(t => t.id === id);
-    if (!task) return;
+    if (!task)
+        return;
 
-    const updatedTask = { ...task, completed: !task.completed };
+    const updatedTask = {...task, completed: !task.completed};
 
     const response = await fetch(API_URL, {
         method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ updatedTask })
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({updatedTask})
     });
 
     const savedTask = await response.json();
@@ -55,14 +56,15 @@ export const toggleTask = (id) => {
 //Editar tarea
 export const editTask = (id, newTask) => {
     const task = tasks.find(t => t.id === id);
-    if (!task) return;
+    if (!task)
+        return;
 
-    const updatedTask = { ...task, text: newText };
+    const updatedTask = {...task, text: newText};
 
     const response = await fetch(API_URL, {
         method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ updatedTask })
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({updatedTask})
     });
     const savedTask = await response.json();
     task.completed = savedTask.completed;
